3e53aa722788ecf6b7df8301a00cbfd2cf847dfe
Kiesha <lol@fake.com>
2016-05-10 14:55:29 +0100

    Latest

--- Script.js
+++ Script.js
@@ -1,12 +1,21 @@
-var greet = function (event_details) {
-    var name = document.getElementById ("name").value;
-    var email = document.getElementById ("email").value;
-    alert ('Thank you, ' + name + ". See you in your inbox soon.");
+var greet;
+greet = function (event_details) {
     event_details.preventDefault();
+    var name = document.getElementById("name").value;
+    //var email = document.getElementById ("email").value;
+    //alert ('Thank you, ' + name + ". See you in your inbox soon.");
+
+    event_details.target.style.display = 'none';
+
+    var container = document.getElementById('contact-form-container');
+    container.innerHTML = "<p>" + name + " has been signed up." + "</p>";
+
+    var tagline = document.getElementById('tagline4');
+    tagline.style.color = "red";
 
 };
 
-var form = document.getElementById('signup-form');
-form.addEventListener ("submit", greet);
 
+var form = document.getElementById('signup-form');
+form.addEventListener("submit", greet);
 
